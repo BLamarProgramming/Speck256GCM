3713d61f63d5948ae1973967be87cb9b2c9746bd
Bailey Lamar <bailey.lamar5@gmail.com>
2025-01-23 15:51:42 -0500

    Added basic file_handling functions to get text from file into byte vector and back.

---
+++ src/file_handling.rs
@@ -0,0 +1,19 @@
+use std::fs::read_to_string;
+use std::fs::File;
+use std::io::Write;
+
+
+// Read file into a byte vector that can then be encrypted or decrypted
+fn read_file_into_byte_vec(path: &str) -> Vec<u8> {
+    let text: String = read_to_string(path).expect("Error in reading the file");
+    let bytes: Vec<u8> = text.as_bytes().to_vec();
+    return bytes
+
+}
+// Writes file from byte vector (encrypted or decrypted)
+fn write_file_from_byte_vec(path: &str, bytes :Vec<u8>){
+    let mut file = File::create(path).expect("Failed to create file.");
+    file.write_all(&bytes).expect("Failed to write to file.");
+    file.flush().expect("Failed to flush file.")
+   
+}
